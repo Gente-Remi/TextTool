758c9833978592b7f82620cb6256e12c61bfb82c
teteit <rereloulo13@gmail.com>
2025-11-24 18:25:41 +0100

    Ajout prefix par C

--- texttool.py
+++ texttool.py
@@ -12,6 +12,8 @@ def process_line(line):
         return text.upper()
     if cmd == "lowercase":
         return text.lower()
+    if command == 'prefix':
+        print(text[:10])
 
     return "Unknown command " + cmd
 
